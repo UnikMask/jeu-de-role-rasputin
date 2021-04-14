c89858916d48e92be2b677299623328977485381
arcreaneDev <dominique@arcreane.com>
2021-04-14 14:33:12 +0200

    continue Code clean up sur maingame.py

--- maingame.py
+++ maingame.py
@@ -3,13 +3,3 @@ import tkinter as tk
 print("Bienvenue dans le jeu dont vous etes le hero ! /n Vous etes actuellement en train de jouer le jeu cree par:",
 
 
-def leftclick():
-    for event in pygame.event.get()
-        if event.type == MOUSEBUTTONDOWN:  # récupération des évenements lié à un clic de souris
-            if event.button == 1:  # parmis ceux précédent je prend le clic gauche
-                x, y = pygame.mouse.get_pos()
-
-    if b > x > a and d > y > c:
-        return True
-    else:
-        return False
