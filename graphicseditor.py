ea45bdab4893ccd3eab6f11abac3ffdef8f605e8
Sh0lf <vyvincentyap@gmail.com>
2021-04-22 17:43:19 +0200

    Successfully implemented csv integration + beginning of items button. Need to implement back button for editor main menu page

--- graphicseditor.py
+++ graphicseditor.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+import csv
+att_update=0
+defense_update=0
+agi_update=0
+luck_update=0
 
 def stats_edit_window():
     stats_edit = tk.Tk()
-    stats_edit.configure(bg='blue')
+    stats_edit.configure(bg='#e9d49c')
     stats_edit.geometry("1080x720")
-    title_stats_edit = tk.Label(stats_edit, text="Modification des stats", bg="blue", font=("Arial", 20, "bold"),
-                                fg="black")
+    title_stats_edit = tk.Label(stats_edit, text="Modification des stats", bg="#e9d49c", font=("Arial", 20, "bold"), fg="black")
     title_stats_edit.pack(side="top", pady=50)
 
     frame_edit_stats = tk.Frame(stats_edit, bg="orange")
@@ -59,19 +63,35 @@ def stats_edit_window():
     spinBox_luck.delete(0)
     spinBox_luck.insert(1, default)
 
-    #def confirmation(): #creation fonction confirmation des points
+    def confirmation():
+        #with open(../jeu_maingame/variables_saves.csv) as csvfile:
+            #writer = csv.write(csvfile)
+            #writer.writerow(att_update)
+            #writer.writerow(defense_update)
+            #writer.writerow(agi_update)
+            #writer.writerow(luck_update)
+        stats_edit.withdraw()
+        editor.deiconify()
 
-    confirm = tk.Button(frame_edit_stats, text="Confirmer vos points des stats ?", font=("Arial", 16), fg="green") #command=confirmation)
+    confirm = tk.Button(frame_edit_stats, text="Confirmer vos points des stats ?", font=("Arial", 16), fg="green", command=confirmation)
     confirm.grid(row=5, column=1, ipady=50, padx=50, pady=50)
 
-    editor.destroy()
+    editor.withdraw()
+
+def items_edit_window():
+    items_edit = tk.Tk()
+    items_edit.configure(bg="#e9d49c")
+    items_edit.geometry("1080x720")
+    tk.Label(items_edit, text="Ajouter des objets dans votre jeu", bg="#e9d49c", font=("Arial", 20, "bold"),fg="black").pack(side="top", pady=50)
+
+    editor.withdraw()
 
 # creation de la fenetre editor tkinter
 editor = tk.Tk()
-editor.configure(bg='#92cbf7')  # Couleur du background en bleu clair
+editor.configure(bg='#2e2b2a')  # Couleur du background en bleu clair
 editor.geometry("1080x720")  # Taille page
 
-title2 = tk.Label(text="Vous etes dans l'editeur du jeu", bg='#92cbf7', font=("Arial", 20, "bold"), fg="black")
+title2 = tk.Label(text="Vous etes dans l'editeur du jeu", bg='#2e2b2a', font=("Arial", 20, "bold"), fg="white")
 title2.pack(side="top", pady=50)
 
 frame1 = tk.Frame(editor, bg='#fc9403')
@@ -82,7 +102,7 @@ photo_stats = tk.PhotoImage(file=r"./Plan-de-travail-6-150x150.png")
 stats = tk.Button(frame1, text="Changer les valeurs des Stats", image=photo_stats, compound="top", command=stats_edit_window)
 stats.grid(row="0", column="0", ipady=10, padx=10, pady=10)
 
-items = tk.Button(frame1, text="Ajouter des items et definir l'inventaire dans votre jeu")  # command=items_edit_window
+items = tk.Button(frame1, text="Ajouter des items et definir l'inventaire dans votre jeu", command=items_edit_window)
 items.grid(row="1", column="0", ipady=10, padx=10, pady=10)
 
 dialogues = tk.Button(frame1, text="Ajouter les dialogues dans votre jeu")  # command=dialogues_edit_window
