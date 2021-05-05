86af2cefc0e72e300c9ea7cba1c2f00a25a95645
Sh0lf <vyvincentyap@gmail.com>
2021-05-05 16:20:53 +0200

    Commit pour version plus recente

--- graphicseditor.py
+++ graphicseditor.py
@@ -21,48 +21,6 @@ def stats_edit_window():
     update_text.set(" ")
     tk.Label(frame_edit_stats, textvariable = update_text, font=("Arial", 12), fg="red").grid(row="5", column="0", ipady=50, padx=50, pady=50)
 
-    def update(): #Update nbr des points restants a mettre dans les stats
-        att_update = int(spinBox_att.get())
-        defense_update = int(spinBox_def.get())
-        agi_update = int(spinBox_agi.get())
-        luck_update = int(spinBox_luck.get())
-        nbr_restants = 50 - att_update - defense_update - agi_update - luck_update
-        if nbr_restants < 0:
-            update_text.set("Vous ne pouvez pas faire ca ! Le nombre de points maximum en tout est 50")
-        else:
-            update_text.set("Il vous reste actuellement: " + str(nbr_restants) + " points")
-
-    default = 10
-
-    #Creation des spinbox pour les stats
-    att = tk.Label(frame_edit_stats, text="Attaque", font=("Arial", 12), fg="black")
-    att.grid(row="0", column="0", ipady=10, padx=10, pady=10)
-    spinBox_att = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
-    spinBox_att.grid(row="0", column="1", ipady=10, padx=10, pady=10)
-    spinBox_att.delete(0)
-    spinBox_att.insert(1, default)
-
-    defe = tk.Label(frame_edit_stats, text="Defense", font=("Arial", 12), fg="black")
-    defe.grid(row="1", column="0", ipady=10, padx=10, pady=10)
-    spinBox_def = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
-    spinBox_def.grid(row="1", column="1", ipady=10, padx=10, pady=10)
-    spinBox_def.delete(0)
-    spinBox_def.insert(1, default)
-
-    agi = tk.Label(frame_edit_stats, text="Agilite", font=("Arial", 12), fg="black")
-    agi.grid(row="2", column="0", ipady=10, padx=10, pady=10)
-    spinBox_agi = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
-    spinBox_agi.grid(row="2", column="1", ipady=10, padx=10, pady=10)
-    spinBox_agi.delete(0)
-    spinBox_agi.insert(1, default)
-
-    luck = tk.Label(frame_edit_stats, text="Chance", font=("Arial", 12), fg="black")
-    luck.grid(row="3", column="0", ipady=10, padx=10, pady=10)
-    spinBox_luck = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
-    spinBox_luck.grid(row="3", column="1", ipady=10, padx=10, pady=10)
-    spinBox_luck.delete(0)
-    spinBox_luck.insert(1, default)
-
     def confirmation():
         #with open(../jeu_maingame/variables_saves.csv) as csvfile:
             #writer = csv.write(csvfile)
@@ -76,6 +34,54 @@ def stats_edit_window():
     confirm = tk.Button(frame_edit_stats, text="Confirmer vos points de stats ?", font=("Arial", 16), fg="green", command=confirmation)
     confirm.grid(row=5, column=1, ipady=50, padx=50, pady=50)
 
+    def update(): #Update nbr des points restants a mettre dans les stats
+        att_update = int(spinBox_att.get())
+        defense_update = int(spinBox_def.get())
+        agi_update = int(spinBox_agi.get())
+        luck_update = int(spinBox_luck.get())
+        nbr_restants = 50 - att_update - defense_update - agi_update - luck_update
+        if nbr_restants < 0:
+            update_text.set("Vous ne pouvez pas faire ca ! Le nombre de points maximum en tout est 50")
+            confirm.config(state="disabled", takefocus=0)
+        else:
+            update_text.set("Il vous reste actuellement: " + str(nbr_restants) + " points")
+            confirm.config(state="active")
+
+    default = 10
+
+    #Creation des spinbox pour les stats
+    att = tk.Label(frame_edit_stats, text="Attaque", font=("Arial", 12), fg="black")
+    att.grid(row="0", column="0", ipady=10, padx=10, pady=10)
+    spinBox_att = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
+    spinBox_att.grid(row="0", column="1", ipady=10, padx=10, pady=10)
+    spinBox_att.delete(0)
+    spinBox_att.insert(1, default)
+    spinBox_att.config(state="readonly")
+
+    defe = tk.Label(frame_edit_stats, text="Defense", font=("Arial", 12), fg="black")
+    defe.grid(row="1", column="0", ipady=10, padx=10, pady=10)
+    spinBox_def = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
+    spinBox_def.grid(row="1", column="1", ipady=10, padx=10, pady=10)
+    spinBox_def.delete(0)
+    spinBox_def.insert(1, default)
+    spinBox_def.config(state="readonly")
+
+    agi = tk.Label(frame_edit_stats, text="Agilite", font=("Arial", 12), fg="black")
+    agi.grid(row="2", column="0", ipady=10, padx=10, pady=10)
+    spinBox_agi = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
+    spinBox_agi.grid(row="2", column="1", ipady=10, padx=10, pady=10)
+    spinBox_agi.delete(0)
+    spinBox_agi.insert(1, default)
+    spinBox_agi.config(state="readonly")
+
+    luck = tk.Label(frame_edit_stats, text="Chance", font=("Arial", 12), fg="black")
+    luck.grid(row="3", column="0", ipady=10, padx=10, pady=10)
+    spinBox_luck = tk.Spinbox(frame_edit_stats, from_=1, to=50, font=('arial', 12, 'normal'), bg='#F0F8FF', width=10, command=update)
+    spinBox_luck.grid(row="3", column="1", ipady=10, padx=10, pady=10)
+    spinBox_luck.delete(0)
+    spinBox_luck.insert(1, default)
+    spinBox_luck.config(state="readonly")
+
     editor.withdraw()
 
 def items_edit_window():
@@ -84,7 +90,23 @@ def items_edit_window():
     items_edit.geometry("1080x720")
     tk.Label(items_edit, text="Ajouter des objets dans votre jeu", bg="#e9d49c", font=("Arial", 20, "bold"),fg="black").pack(side="top", pady=50)
     frame_items = tk.Frame(items_edit, bg="#e9d49c")
-    armes = tk.Button(frame_items, text="Ajouter des armes")
+    frame_items.pack()
+
+    def add_accessoires():
+        accessoire_window = tk.Tk()
+        accessoire_window.configure(bg='#e9d49c')
+        accessoire_window.geometry("1080x720")
+        title_accessoire_window = tk.Label(accessoire_window, text="Ajout accessoire", bg="#e9d49c", font=("Arial", 20, "bold"),fg="black")
+        title_accessoire_window.pack(side="top", pady=50)
+
+        frame_accessoire_window = tk.Frame(accessoire_window, bg="orange")
+        frame_accessoire_window.pack()
+
+    armes = tk.Button(frame_items, text="Ajouter des armes", bg="white", font=("Arial", 12, "normal"), fg="black") #, command=add_armes)
+    armes.grid(row=0, column=0, ipady=50, padx=50, pady=50)
+
+    accessoires = tk.Button(frame_items, text="Ajouter des accessoires", bg="white", font=("Arial", 12, "normal"), fg="black", command=add_accessoires)
+    accessoires.grid(row=1, column=0, ipady=50, padx=50, pady=50)
 
     editor.withdraw()
 
